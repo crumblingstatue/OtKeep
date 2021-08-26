613fa948db73d8d51f8e7d76b84db3bb628c47a1
crumblingstatue <radiantstatue@gmail.com>
2021-08-26 22:13:44 +0200

    Use cmd for scripts on windows

--- src/run.rs
+++ src/run.rs
@@ -10,5 +10,20 @@ pub(crate) fn run_script(
     let temp_dir = temp_dir::TempDir::new()?;
     let path = temp_dir.child("script");
     std::fs::write(&path, script)?;
-    Ok(Command::new("sh").arg(path).args(args).status()?)
+    Ok(script_command(|cmd| cmd.arg(path).args(args).status())?)
+}
+
+type CmdResult = std::io::Result<ExitStatus>;
+
+#[cfg(not(target_os = "windows"))]
+fn script_command<F: FnOnce(&mut Command) -> CmdResult>(f: F) -> CmdResult {
+    let mut cmd = Command::new("sh");
+    f(&mut cmd)
+}
+
+#[cfg(target_os = "windows")]
+fn script_command<F: FnOnce(&mut Command) -> CmdResult>(f: F) -> CmdResult {
+    let mut cmd = Command::new("cmd");
+    cmd.arg("/c");
+    f(&mut cmd)
 }
